5ae248570bd142e0b3313df370fa6a58c90c2b6d
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-10-09 21:16:10 -0600

    Create DriveForward.ino

---
+++ MechatronicsMain/DriveForward.ino
@@ -0,0 +1,14 @@
+void driveForward(int M, double t) {
+  md.setM1Speed(-M);
+  md.setM2Speed(-M);
+  delay(2000);
+  md.setM1Speed(0);
+  md.setM2Speed(0);
+  
+  // long position = encLeft.read();
+  // if (position >= 500){
+  //   md.setSpeed(0);
+  //   endLeft.write(0);
+  //   Serial.println("Robot has driven forward.")
+  
+}
