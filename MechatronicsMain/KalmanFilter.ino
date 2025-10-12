45547a27594fad5e2771882f8d932e5b7ef0ef6a
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-10-11 20:39:37 -0600

    Update KalmanFilter.ino
    10/11 Working filter i think

--- MechatronicsMain/KalmanFilter.ino
+++ MechatronicsMain/KalmanFilter.ino
@@ -1,51 +1,59 @@
-import <BasicLinearAlgebra.h> using namespace BLA;
+#import <BasicLinearAlgebra.h> using namespace BLA;
+float kalmanFilter(float z){
+  static BLA::Matrix<2,1> x = {0,0};
+  static BLA::Matrix<2,2> P = {0.01,0,0,0.01};
+  static const float dt = 0.01f;
+  static const float R = 0.5;
+  static BLA::Matrix<2,2> Qk = {0.004,0,0,0.004};
+  static BLA::Matrix<2,2> F = {1,dt,0,1};
+  static BLA::Matrix<1,2> H = {1,0};
+  static BLA::Matrix<2,1> B = {0,dt};
+  static BLA::Matrix<2,2> I = {1,0,0,1};
+  float u = 0.5;
 
-BLA::Matrix<2,2> F,P,Qk, I;
-BLA::Matrix<2,1> B, K,
-BLA::Matrix<1,2> H;
-float R = 0.5f;
-float u = 0.5f;
-float dt = 0.01f;
+  x = F * x + B * u;
+  P = F * P * (~F) + Qk;
 
-const int step = 100;
-float u = 0.5f;
-float dt = 0.001;
+  // Update
+  float S = (H * P * (~H))(0,0) + R;
+  BLA::Matrix<2,1> K = (P * (~H)) / S;
+  x = x + K * (z - (H * x)(0,0));
+  P = (I - K * H) * P;
 
-int X = [step];
-
-int filteredDistance = [step];
-int predictedDistance = [step];
-
-void kalmanFilter(){
-
-F = {1,dt,0,1};
-H = {1,0};
-//B = {0,dt};
-
-
-x[:,1] = (X[1],0);
-//Gatering 100 datapoints
-for (int i = 0; i < step; i++) {
-X[i] = analogRead(A0) / 1023.0 * 5.0;  // example sensor read - maybe map
+  return x(0,0);
 }
 
-for (int k = 1; k <= n; n++){
-preditedDistance[:,k] = F * filteredDistance[:,k-1]; //+ B * u;
-P_pred = F*P*F.T + Qk;
+  // Use to test how long it takes to compute
+  // uint32_t t0 = micros();
+  // for (int i = 0; i < 100; ++i) {
+  //   kalmanUpdate(fakeMeasurement());  // your KF
+  // }
+  // uint32_t dt100 = micros() - t0;
+  // Serial.print("Per update (us): ");
+  // Serial.println(dt100 / 100.0);
 
-Y = X(k);
-K = (P_pred*H.T)/(H*P_pred*H.T+R);
 
-P = (eye(2), K.H)*P_pred;
-filteredDistance = predictedDistance[:,k] + K*(Y-H*predictedDistance[:,k]);
+  // Origional
 
-}
+  // const int step = 100;
+  // float X[step];
+  // float filteredDistance[step];
+  // float predictedDistance[step];
 
-// Use to test how long it takes to compute
-// uint32_t t0 = micros();
-// for (int i = 0; i < 100; ++i) {
-//   kalmanUpdate(fakeMeasurement());  // your KF
-// }
-// uint32_t dt100 = micros() - t0;
-// Serial.print("Per update (us): ");
-// Serial.println(dt100 / 100.0);
+  //   //B = {0,dt};
+  // predictedDistance[:,1] = (X[:,1],0);
+
+  // //Gatering 100 datapoints
+  // for (int i = 0; i < step; i++) {
+  // X[i] = analogRead(IRpin) / 1023.0 * 5.0;  // example sensor read - maybe map
+  // }
+
+  // for (int k = 1; k <= n; n++){
+  // preditedDistance[:,k] = F * filteredDistance[:,k-1]; //+ B * u;
+  // P_pred = F*P*F.T + Qk;
+
+  // Y = X(k);
+  // K = (P_pred*H.T)/(H*P_pred*H.T+R);
+  // P = (I, K.H)*P_pred;
+
+  // filteredDistance[:,k] = predictedDistance[:,k] + K*(Y-H*predictedDistance[:,k]);
