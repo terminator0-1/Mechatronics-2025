217529b8585390263d3a356fa1260bc5586a0a3c
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-10-11 20:38:27 -0600

    Update DriveForward.ino

--- MechatronicsMain/DriveForward.ino
+++ MechatronicsMain/DriveForward.ino
@@ -1,9 +1,9 @@
 void driveForward(int M, double t) {
   md.setM1Speed(-M);
   md.setM2Speed(-M);
-  delay(2000);
-  md.setM1Speed(0);
-  md.setM2Speed(0);
+  // delay(2000);
+  // md.setM1Speed(0);
+  // md.setM2Speed(0);
   
   // long position = encLeft.read();
   // if (position >= 500){
