d3515d762b93778a9e9de66b9b2d4194debcd819
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-10-14 17:00:27 -0600

    Update MoveArmD.ino
    fixed direction and added switch functionality

--- MechatronicsMain/MoveArmD.ino
+++ MechatronicsMain/MoveArmD.ino
@@ -1,5 +1,13 @@
 void moveArmD(){
-  armMotor.forward();
-  delay(1500);
-  armMotor.stop();
-}
+  armMotor.backward();
+   //   bool armNotHome = true;
+   //   while (armNotHome){
+   //      if (digitalRead(homeSwitch) == HIGH){
+   //     armMotor.stop();
+   //     armNotHome = false;
+   //      }
+   //   }
+   delay(1500);
+   armMotor.stop();
+  }
+   
