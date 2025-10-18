35df039d93bca8c26eb11fd5df653c1d67e10bb0
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-10-17 20:37:27 -0600

    Update getWood.ino

--- MechatronicsMain/getWood.ino
+++ MechatronicsMain/getWood.ino
@@ -1,1 +1,18 @@
+void getWood(){
+  
+// Assuming start is -- backed up ~10cm from the crafting table. 
 
+// Follow Wood Trajectory/Path
+
+// Count lines if detected
+int lineCount = 0;
+while (lineCount < 2){
+  qtr.sense();
+  for all senseVals
+    if senseval1 & senseval2 & senseval3... = target{
+      count ++
+    }
+  
+
+} 
+}
