3be6d7684dca797d7d2c8e496f40a1234aae7d89
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-11-07 11:57:16 -0700

    Refactor switchState function for Serial2 and command handling

--- MechatronicsMain/switchState.ino
+++ MechatronicsMain/switchState.ino
@@ -1,61 +1,43 @@
 void switchState() {
-  if (Serial1.available()) {
 
-    String dataString = Serial1.readStringUntil('\n');
+
+  if (Serial2.available()) {
+    String dataString = Serial2.readStringUntil('\n');
     dataString.trim();
-    
-    if (dataString.length() == 1) {
+    if (dataString.length() <= 7) {
       char Signal = dataString.charAt(0);
-    
-      Serial.println(Signal);
+
       switch (Signal) {
-        case 'a':
-          driveForward(M, M);
-          Serial.println("Driving Forward...");
-          break;
-
-        case 'b':
-          driveBackward(M);
-          Serial.println("Driving Backward...");
-          break;
-
-        case 'l':
-          turnLeft(M);
-          Serial.println("Turning Left...");
-          break;
-        case 'r':
-          turnRight(M);
-          Serial.println("Turning Right...");
-          break;
-        case 'u':  // maybe shorthand for "craft"
+        case 'u':
           moveArmU();
-          Serial.println("Moving Arm Up.");
           break;
-
         case 'd':
           moveArmD();
-          Serial.println("Moving Arm Down.");
           break;
-        case 's':
-          myServo1.write(100);
-          // myServo2.write(150);
-          // myServo3.write(150);
-          delay(250);
-          myServo1.write(0);
-          // myServo2.write(0);
-          // myServo3.write(0);
-          Serial.println("Servo Moved");
+        case 'r':
+          harvestBlock();
+          // Serial2.println("Sensed block: Iron");
           break;
-        case 'c':
-          // Start LineFollowing
-          followLine();
+        case 'm':
+          dispenceBlock();
           break;
         case 'w':
-          // Measure distance towards wall
-          Serial.println("Hello");
-          float dist = 20.0;
-          driveForward(M,M);
-          readWall(dist);
+          readWall(15.0);
+          break;
+        case 's':
+          myServo1.write(180);
+          delay(500);
+          myServo1.write(0);
+        case 'x':
+          // Stop Driving Motors
+          md.setSpeeds(0, 0);
+          // Stop Arm Motor
+          armMotor.stop();
+          // Set servos to 0.
+          myServo1.write(0);
+          myServo2.write(0);
+          myServo3.write(0);
+          //set defaultState = true;
           break;
       }
     }
