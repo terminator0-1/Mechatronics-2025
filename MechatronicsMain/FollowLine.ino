f487d3302f4670563a10d0fefac167dbfcccd61e
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-12-01 10:56:32 -0700

    Refactor followLine function to include parameters

--- MechatronicsMain/FollowLine.ino
+++ MechatronicsMain/FollowLine.ino
@@ -1,62 +1,78 @@
-void followLine() {
-alpha = 0.7;
-    double t, t_old, deltaT, print_time, t0d = 0;  // declare some time variables
+void followLine(bool pillarFlag, float basespeed) {
+  double t, print_time = 0;
+  float dError, lasterror;
+  unsigned long lastMotorUpdate = 0;
+  bool following = true;
+  unsigned long distStart = millis();
+  int distDelay = 6800; // 6800 @ 200 speed
+
+  // ---------------------------------------------------
+  // FIX 1: Reset PID state so no giant first correction
+  error = 0;
+  lasterror = 0;
+  dError = 0;
+
+  // FIX 2: Clear prevDetected flag
+  prevDetected = false;
+
+  // FIX 3: Stabilize QTR before starting PID
+  for (int k = 0; k < 10; k++) {
+    qtr.read(sensorValues);
+    for (int i = 0; i < 8; i++) {
+      Sensor_value_unbiased[i] = sensorValues[i] - sensor_bias[i];
+    }
+    delay(2);
+  }
+  // ---------------------------------------------------
+
   while (following) {
-    t = micros() / 1000000. - t0;
-    qtr.read(sensorValues);  
-    deltaT = t-t_old; 
+    // --- Time update ---
+    t = micros() / 1000000.0 - t0;
+
+    // --- Read QTR sensors ---
+    qtr.read(sensorValues);
+
     for (int i = 0; i < 8; i++) {
       Sensor_value_unbiased[i] = sensorValues[i] - sensor_bias[i];
     }
 
+    // --- Compute weighted line position ---
     dTop = 0;
     dBottom = 0;
+
     for (int i = 0; i < 8; i++) {
-      dTop = dTop + Sensor_value_unbiased[i] * d[i];
-      dBottom = dBottom + Sensor_value_unbiased[i];
+      dTop += Sensor_value_unbiased[i] * d[i];
+      dBottom += Sensor_value_unbiased[i];
     }
-    
+
+    // Compute Error
     dComp = dTop / dBottom;
-      
-    error1 = d0 - dComp;
-    error2 = dComp - d0;
+    error = d0 - dComp;
 
-    omega1f = alpha*omega1 + (1-alpha) * omega1f;
-    omega2f = alpha*omega2 + (1-alpha) * omega2f;
+    Serial.println(error);
 
-    omega1_des = 0;
-    omega2_des = 0;
+    dError = error - lasterror;
+    lasterror = error;
 
-    dErrordt1 = omega1_des - omega1f;
-    dErrordt2 = omega2_des - omega2f;
+    // --- Compute motor commands ---
+    int m1c = basespeed + (Kp * error + Kd * dError);
+    int m2c = basespeed - (Kp * error + Kd * dError);
+    md.setSpeeds(m1c, m2c);
 
-    Kerror = (Kp * error) + (Ki * integralError) + Kd*dErrordt;
-      
-    V2 = basespeed + Kerror;
-    V1 = basespeed - Kerror;
-
-    //prevError = error; // PID Control
-
-    ////////// LINE FOLLOWING CODE
-    md.setM1Speed(V1);  // send motor commands
-    md.setM2Speed(V2);
-
-    // PRINT STATEMENTS
-    // non-blocking way to delay printing
-    if ((t - print_time) > 0.25) {
-
-      // This for loop is used to print out variables that are arrays
-      for (uint8_t i = 0; i < SensorCount; i++) {
-        Serial.print(Sensor_value_unbiased[i]);
-        Serial.print('\t');
-      }
-      // Print any non-array variables here
-      Serial.println(error);
-      print_time = t;
+    // 1️⃣ HIT SWITCH
+    if (digitalRead(hitSwitchPin) == 1) {
+      md.setSpeeds(0, 0);
+      Serial.println("Hit Switch Pressed → STOP");
+      following = false;
+      break;
     }
-    // if (readWallLine(dist) == false){
-    //   driveForward(0,0);
-    //   following = false;
-    //}
+
+    if ((millis() - distStart > distDelay) && (pillarFlag == true)) {
+      md.setSpeeds(0, 0);
+      delay(250);
+      straightTrajectory(-10, 1);
+      following = false;
+      break;
+    }
   }
 }
