d0cf19effe17121d8bf1bdc07a9e26cfbab11a53
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-12-01 11:04:42 -0700

    Refactor mineServo to use hits parameter
    Refactor mineServo function to accept hits as parameter and adjust servo positions.

--- MechatronicsMain/mineServo.ino
+++ MechatronicsMain/mineServo.ino
@@ -1,38 +1,13 @@
-void mineServo(char sensedColor) {
-  
-  int hits = 0;
-  unsigned long hitTime = 130;
-  // Serial2.println("Enter block (y,r,b): ");
-  // while (!Serial2.available());
-  // String block = Serial2.readStringUntil('\n');
-  // block.trim();
-  // char blockType = block.charAt(0);
+void mineServo(int hits) {
+  unsigned long hitTime = 150;
 
-  switch (sensedColor) {
-    case 'y': hits = 3; break;
-    case 'r': hits = 5;  break;
-    case 'b': hits = 10;  break;
-    default:
-      Serial2.println("Invalid block type.");
-      return;
-  }
-  Serial2.println("Mining...");
-    for (int i = 0; i < hits; i++) {
-    unsigned long startTime = millis();  // mark start of hit
-
-    myServo3.write(-30);
+  for (int i = 0; i < hits; i++) {
+    myServo3.write(-60);
     delay(hitTime);
-    myServo3.write(30);
+    myServo3.write(60);
     delay(hitTime);
-
-    // unsigned long elapsed = millis() - startTime;  // total hit duration
-    // Serial2.print("Hit #");
-    // Serial2.print(i + 1);
-    // Serial2.print(" took ");
-    // Serial2.print(elapsed);
-    // Serial2.println(" ms");
+    Serial.println("Servo Hit");
   }
-
-  myServo3.write(0); // stop in neutral
+  myServo3.write(0);  // stop in neutral
   Serial2.println("Mining complete.");
 }
