5ab17056d3f6e21566d557056926d3d0b41b8e70
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-12-01 10:55:24 -0700

    Refactor dispenseBlock function for clarity

--- MechatronicsMain/dispenseBlocks.ino
+++ MechatronicsMain/dispenseBlocks.ino
@@ -1,17 +1,23 @@
-void dispenceBlock(){
-// Assume driving up to table
+void dispenceBlock() {
+  // Assume driving up to table
+  bool waitSwitch = true;
+  // 1️⃣ HIT SWITCH PRESSED
+  while (waitSwitch) {
+    if (digitalRead(hitSwitchPin) == 1) {
+      Serial2.println("SWITCH HIT");
+      md.setSpeeds(0, 0);
+      straightTrajectory(-8, .5);
+      Serial.println("Hit Switch Pressed → STOP");
+      waitSwitch = false;
+      break;
+    }
+  }
 
-// md.setSpeeds(100,100);
-// readWall(4.5);
-
-// The robot is ready to extend
-moveArmU();
-//Drop block and return.
-delay(500);
-myServo1.write(180);
-delay(300);
-myServo1.write(0);
-moveArmD();
-
-// Sequence finished.
+  moveArmU();
+  delay(250);
+  myServo1.write(180);
+  delay(500);
+  myServo1.write(0);
+  delay(250);
+  moveArmD();
 }
