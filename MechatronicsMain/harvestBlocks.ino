5b813112d48889ca627ad98cf77b7866ad4a9d53
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-12-01 11:02:51 -0700

    Refactor harvestBlock function for enhanced logic

--- MechatronicsMain/harvestBlocks.ino
+++ MechatronicsMain/harvestBlocks.ino
@@ -1,20 +1,62 @@
-void harvestBlock(){
-// Placed infront of pillar 
-readColorSensor(s2, s3, readPin);
+void harvestBlock(char pick) {
+  bool waitSwitch = true;
+  bool silverFishKilled = false;
+  int attempt = 0, collected = 0;
+  // Start
+  followLine(true, 250);
 
-// Once Block has been mined, move into position.
-straightTrajectory(-2.5,.25);
-// Move back in place
+  orient();
 
-// Once it has moved back into place we will delay for 1 second to wait
-//senseMagnetic();
-delay(2000);
+  // 1️⃣ HIT SWITCH PRESSED
+  while (waitSwitch) {
+    if (digitalRead(hitSwitchPin) == 1) {
+      Serial2.println("SWITCH HIT");
+      md.setSpeeds(0, 0);
+      straightTrajectory(-2.25, .25);
+      Serial.println("Hit Switch Pressed → STOP");
+      waitSwitch = false;
+      break;
+    }
+  }
 
-// Move Servo over
-myServo2.write(70);
-delay(200);
-myServo2.write(0);
+  while (collected < 3) {
+    delay(500);
+    // If mining mode, dont reject blocks, instead sense for silverfish.
+    char sensedColor = readColor(s2, s3, readPin);
+    bool reject = determineRejection(sensedColor,pick);
+    int finalHits = getHits(sensedColor, reject, collected, pick);
+    mineServo(finalHits);
 
-// Block should now be collected and moved to the right. 
-// blockCount += 1;
+    straightTrajectory(-4, .25);
+    delay(1000);
+
+    if(reject == true){
+      spitBlock();
+    } else{
+    }
+    // Serial2.println(attempt);
+    // Serial2.println(collected);
+    // Move back in place only for first and second block.
+
+    if (reject == true) {
+      attempt += 1;
+    } else {
+      collected += 1;
+    }
+    if (collected <= 2 && reject == false) {
+      delay(500);
+      myServo2.write(50);
+      delay(500);
+      myServo2.write(0);
+      // Harvest again.
+      if (silverFishKilled == false) {
+        straightTrajectory(4, .25);
+      }
+    }
+  }
+
+  Serial2.println("Finished MINING. Going to table");
 }
+
+// Block should now be collected and moved to the right.
+// blockCount += 1;
