46b1b6c604f46e71a9364ac1837e7ce8786bfd6a
Logan Friel <140680147+terminator0-1@users.noreply.github.com>
2025-12-01 10:53:48 -0700

    Refactor circleTrajectory function parameters and logic

--- MechatronicsMain/circleTrajectory.ino
+++ MechatronicsMain/circleTrajectory.ino
@@ -1,23 +1,15 @@
-void circleTrajectory(double &theta1_final, double &theta2_final, float &travelTime){
-Serial2.println("Enter Circle Radius (cm): ");
-while (!Serial2.available());
-float circleRadius = Serial2.parseFloat();
-Serial2.readStringUntil('\n');  // clear buffer
+void circleTrajectory(float circleRadius, float travelAngle, float travelTime) {
 
-Serial2.println("Enter Travel Angle (deg): ");
-while (!Serial2.available());
-float travelAngle = Serial2.parseFloat() * (PI/180) * 1.08;
-Serial2.readStringUntil('\n');  // clear buffer
+  float theta1_final = 0, theta2_final = 0;
+  int m1backwardsFlag = 1, m2backwardsFlag = 1;
+  char spinFlag = 'n';
 
-Serial2.println("Enter Velocity (cm/s): ");
-while (!Serial2.available());
-travelTime = (travelAngle * circleRadius) / Serial2.parseFloat();
-Serial2.readStringUntil('\n');  // clear buffer
-
-//Both thetas are positive.
-theta1_final = (travelAngle * circleRadius) / rw;
-theta2_final = (travelAngle * (circleRadius + D)) / rw;
-// theta1_final = (travelAngle * (circleRadius/2)) / rw;
-// theta2_final = (travelAngle * ((circleRadius/2) * D)) / rw;
-
+  if (travelAngle > 0) {
+    theta1_final = -((travelAngle * (PI / 180.0) * 1.05) * (circleRadius + D)) / rw;
+    theta2_final = -((travelAngle * (PI / 180.0) * 1.05) * circleRadius) / rw;
+  }else{
+  theta1_final = ((travelAngle * (PI / 180.0) * 1.05) * circleRadius) / rw;
+  theta2_final = ((travelAngle * (PI / 180.0) * 1.05) * (circleRadius + D)) / rw;
+  }
+  getTrajectory(theta1_final, theta2_final, travelTime, spinFlag, m1backwardsFlag, m2backwardsFlag);
 }
